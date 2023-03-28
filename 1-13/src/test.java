38f23757587cd8dff8c179d88ff0c47412dd41eb
tolbou <ttorubou@gmail.com>
2023-03-29 01:41:12 +0900

    1-13課題再提出

--- 1-13/src/test.java
+++ 1-13/src/test.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -6,53 +5,53 @@ import java.util.List;
 public class test {
 	public static void main(String[] args) {
 		// 改行
-		System.out.println("問１");
-		// 問題1:下記の条件を使ってfor文と条件文を組み合わせ結果が同じになるよう出力しなさい。for文ネスト3回（3回forを書くこと）
+		System.out.println("問1");
+
 		// strArray1でfor文を回し3文字目(k)以降のものをstrArray2に格納する(1個目のfor文)
 		// strArray2でfor文を回しkとaとoの物だけでstrArray3に格納する(2個目のfor文)
 		// strArray3でfor文を回し「kakao」となるようにstrArray4に格納しstrArray4を出力する。(3個目のfor文)
+
 		List<String> strArray1 = new ArrayList(
 				Arrays.asList("f", "u", "k", "u", "d", "a", "k", "a", "n", "e", "k", "o"));
-		List<String> strArray2 = new ArrayList<String>();
-		List<String> strArray3 = new ArrayList<String>();
-		List<String> strArray4 = new ArrayList<String>();
+		List<String> strArray2 = new ArrayList<>();
+		List<String> strArray3 = new ArrayList<>();
+		List<String> strArray4 = new ArrayList<>();
 
+		outerLoop:
+		for (int i = 2; i < strArray1.size(); i++) {
+			strArray2.add(strArray1.get(i));
+			for (int j = 0; j < strArray2.size(); j++) {
+				if (strArray2.get(j).equals("k") || strArray2.get(j).equals("a") || strArray2.get(j).equals("o")) {
+					strArray3.add(strArray2.get(j));
+					for (int k = 0; k < strArray3.size(); k++) {
+						if (strArray3.get(k).equals("k") && strArray4.size() == 0) {
+							strArray4.add(strArray3.get(k));
+						} else if (strArray3.get(k).equals("a") && (strArray4.size() == 1 || strArray4.size() == 3)) {
+							strArray4.add(strArray3.get(k));
+						} else if (strArray3.get(k).equals("k") && strArray4.size() == 2) {
+							strArray4.add(strArray3.get(k));
+						} else if (strArray3.get(k).equals("a") && strArray4.size() == 4) {
+							strArray4.add(strArray3.get(k));
+						} else if (strArray3.get(k).equals("o") && strArray4.size() == 5) {
+							strArray4.add(strArray3.get(k));
+							break outerLoop;
+						}
+					}
+				}
+			}
+			strArray3.clear();
+			strArray4.clear();
+		}
 
-        for (int i = 2; i < strArray1.size(); i++) {
-            strArray2.add(strArray1.get(i));
-        }
+		if (!strArray4.isEmpty() && strArray4.size() == 6) {
+			strArray4.remove(4);
+		}
 
-        for (int i = 0; i < strArray2.size(); i++) {
-            if (strArray2.get(i).equals("k") || strArray2.get(i).equals("a") || strArray2.get(i).equals("o")) {
-                strArray3.add(strArray2.get(i));
-            }
-        }
+		if (strArray4.isEmpty()) {
+			System.out.println("Not found");
+		} else {
+			System.out.println(strArray4);
+		}
 
-        outerLoop:
-        for (int i = 0; i < strArray3.size(); i++) {
-            if (strArray3.get(i).equals("k")) {
-                for (int j = i + 1; j < strArray3.size(); j++) {
-                    if (strArray3.get(j).equals("a")) {
-                        for (int k = j + 1; k < strArray3.size(); k++) {
-                            if (strArray3.get(k).equals("k")) {
-                                for (int l = k + 1; l < strArray3.size(); l++) {
-                                    if (strArray3.get(l).equals("a")) {
-                                        for (int m = l + 1; m < strArray3.size(); m++) {
-                                            if (strArray3.get(m).equals("o")) {
-                                                strArray4.addAll(Arrays.asList(strArray3.get(i), strArray3.get(j), strArray3.get(k), strArray3.get(l), strArray3.get(m)));
-                                                break outerLoop;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        System.out.println(strArray4);
-    }
-
+	}
 }
